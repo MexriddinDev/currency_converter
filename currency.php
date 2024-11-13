99b9be51ad16dc95f5a2ea244f8c665f68302f31
https://github.com/MexriddinDev <mekkinuriddinov@gmail.com>
2024-11-13 12:55:57 +0500

    frontent-part

--- currency.php
+++ currency.php
@@ -1,6 +1,7 @@
 <?php
-class currency {
+class Currency {
     const CURRENCY_API_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
+    public array $currencies = [];
 
     public function __construct() {
         $ch = curl_init();
@@ -10,9 +11,22 @@ class currency {
         $output = curl_exec($ch);
         curl_close($ch);
 
-        $decoded = json_decode($output);
-        echo $decoded[0]->Ccy;
+        $this->currencies = json_decode($output);
+    }
+
+    public function getCurrencies() {
+        $separated_data = [];
+        $currencies_info = $this->currencies;
+        foreach ($currencies_info as $currency) {
+            $separated_data[$currency->Ccy] = $currency->Rate;
+        }
+        return $separated_data;
+    }
+
+    public function exchange($value, $currency_name = 'USD') {
+       
+        echo ceil($value /$this->getCurrencies()[$currency_name]) . ' ' . $currency_name;
+        
     }
 }
 
-new currency();
